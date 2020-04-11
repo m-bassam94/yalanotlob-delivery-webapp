ebc0957a7ba6784a4a286ea8edec1dda29f398b8
aya morsi <ayamorsi163@gmail.com>
2020-04-11 19:15:00 +0200

    forgot password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+}
+
+config.action_mailer.default_url_options = { :host => 'localhost.herokuapp.com' }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
