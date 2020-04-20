04d688a6dd37f135d4bd5da893e46895fb4c5063
MahaAmin <mahaamin97@gmail.com>
2020-04-20 17:41:07 +0200

    Basic Orders Layout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,27 @@
 class OrdersController < ApplicationController
 
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
     def create
-        redirect_to details_path
+        @order = Order.new
+        @order.user_id = current_user.id
+        @order.save
+        redirect_back(fallback_location: root_path)
+    end
+
+    def update
+    end
+
+    def destroy
     end
 end
