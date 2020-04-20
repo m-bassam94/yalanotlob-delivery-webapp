2def320a0e29ef5700a4a4f218a9504905dbc2a2
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-04-20 23:32:14 +0200

    fixing friends routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :groups
   resources :groups_users
   resources :orders
+  resources :friends
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
