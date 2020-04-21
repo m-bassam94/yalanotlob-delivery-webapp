1ce074ff38984d07ba45eae1e198c4a3e136b5e6
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-04-21 22:17:03 +0200

    latest 5 order on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = Order.where(user_id: current_user.id).limit(10)
+    @orders = Order.where(user_id: current_user.id).limit(5)
   end
 
   def show
