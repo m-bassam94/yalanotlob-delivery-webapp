83252d27f1ac40da3da51f9b6008262516619113
m-bassam94 <mohamedbassam094@gmail.com>
2020-04-22 09:57:04 +0200

    "add order" form validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  validates :orderType, presence: true
+  validates :resturant, presence: true
+
   belongs_to :user
   has_and_belongs_to_many :users
   has_one_attached :menu
